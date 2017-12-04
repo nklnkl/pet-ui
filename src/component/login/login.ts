27d14e0fdbc887280ca28ccf476c2093d6bd79ca
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-03 22:24:14 -0500

    Add 2 way data binding and event handling to login page

--- src/component/login/login.ts
+++ src/component/login/login.ts
@@ -6,4 +6,9 @@
 class LoginComponent {
   private email: string;
   private password: string;
+
+  public login() : void {
+    // this does nothing yet.
+    // HttpService.login(email, password); <= async result
+  }
 }
