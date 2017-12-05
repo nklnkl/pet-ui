10b031709434a4f83dfae72524a812fea0609e16
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-05 06:50:16 -0500

    Add application router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { AccountComponent } from '../component/account/account';
+import { LoginComponent } from '../component/login/login';
+import { PetComponent } from '../component/pet/pet';
+import { SignUpComponent } from '../component/sign-up/sign-up';
+import { NewPetComponent } from '../component/new-pet/new-pet';
+import { PetsComponent } from '../component/pets/pets';
+
+const routes: Routes = [
+  { path: 'account', component: AccountComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'pet/:id', component: PetComponent },
+  { path: 'new-pet', component: NewPetComponent },
+  { path: 'pets', component: PetsComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: '', redirectTo: '/pets', pathMatch: 'full'}
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    LoginComponent,
+    PetComponent,
+    SignUpComponent,
+    NewPetComponent,
+    PetsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
