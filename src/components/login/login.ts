f135f30fe46ff0582179d967940e9c62e90684e0
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-06 14:21:23 -0500

    Add functionality for Login page

--- src/components/login/login.ts
+++ src/components/login/login.ts
@@ -1,15 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import { HttpService } from '../../app/http.service';
 @Component({
   selector: 'login',
   templateUrl: 'login.html',
   styleUrls: ['login.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   private email: string;
   private password: string;
+  private signedUp: boolean;
+  private error: boolean;
+  private errorTitle: string;
+  private errorMessage: string;
+
+  constructor (private route: ActivatedRoute, private service: HttpService,
+   private router: Router) {
+    this.signedUp = false;
+    this.error = false;
+  }
+
+  ngOnInit () {
+    this.route.params.subscribe((params: Params) => {
+      this.signedUp = params.signedUp;
+    });
+  }
 
   public login() : void {
-    // this does nothing yet.
-    // HttpService.login(email, password); <= async result
+    if (!this.email || !this.password) {
+      this.error = true;
+      this.errorTitle = 'Error!';
+      this.errorMessage = 'Please enter your email, password, and confirm your password.';
+      return;
+    }
+    this.service.login(this.email, this.password)
+    .subscribe((result: number) => {
+      if (result == 1 ) {
+        this.error = true;
+        this.errorTitle = 'Error!';
+        this.errorMessage = 'Please enter your email, password, and confirm your password.';
+        return;
+      }
+      if (result == 2 ) {
+        this.error = true;
+        this.errorTitle = 'Error!';
+        this.errorMessage = 'We have encountered a server error, please try again later.';
+        return;
+      }
+      this.router.navigate(['pets']);
+      return;
+    });
   }
 }
